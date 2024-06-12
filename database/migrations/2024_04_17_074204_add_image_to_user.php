b461a750bf8f9281a9f071ecf286d7eaeac41836
Alejandro Gómez <alejgom02@gmail.com>
2024-06-12 12:50:00 +0200

    migration changed add default image bug fix

--- database/migrations/2024_04_17_074204_add_image_to_user.php
+++ database/migrations/2024_04_17_074204_add_image_to_user.php
@@ -14,7 +14,7 @@ class AddImageToUser extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('profile_image')->default('../storage/profile_images/app/public/profile_images/default.png')->after('password');
+            $table->string('profile_image')->default('../storage/images/default.png')->after('password');
         });
     }
 
