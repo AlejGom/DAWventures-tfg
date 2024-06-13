eb5001f95bb1ae37b9327536bf594eea539f8c72
Alejandro Gómez <alejgom02@gmail.com>
2024-06-13 15:29:46 +0200

    create rol column
    add admin on migration

---
+++ database/migrations/2024_06_13_132830_add_admin_to_users.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            DB::table('users')->insert([
+                'name'       => 'admin',
+                'email'      => 'admin@admin.es',
+                'password'   => Hash::make('adminadmin'),
+                'rol'        => 'admin',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
